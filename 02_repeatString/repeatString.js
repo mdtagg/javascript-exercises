ea25347cd0083876f1ad3cb0fc0b129ae624cfe6
Mike Tagg <mdtagg91@gmail.com>
2022-09-30 17:11:26 -0700

    exercise 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string,times) {
+    let repeatedString = []
+    if(times >= 0) {
+        for(let i = 0;i < times;i++) {
+            repeatedString.push(string)
+        }
+        return repeatedString.join('')
+    }else {
+        return 'ERROR'
+    }
+    
 };
 
 // Do not edit below this line
