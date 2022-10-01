032a21b5438d018011352d52f9f3389c3d30005e
Mike Tagg <mdtagg91@gmail.com>
2022-10-01 12:20:57 -0700

    Exercise 9 complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function(...numbers) {
+  let sum = numbers.reduce((previousValue,currentValue) => 
+    previousValue + currentValue
+  )
+  return sum
 };
 
-const subtract = function() {
-	
+const subtract = function(...numbers) {
+  let subtracted = numbers.reduce((previousValue,currentValue) => 
+    previousValue - currentValue
+  )
+  return subtracted
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let initialValue = 0;
+  let sum = numbers.reduce((previousValue,currentValue) => 
+    previousValue + currentValue,initialValue
+  )
+  return parseInt(sum)
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let quotient = numbers.reduce((previousValue,currentValue) => 
+  previousValue * currentValue
+  )
+  return quotient
 };
 
-const power = function() {
-	
+const power = function(firstNumber,secondNumber) {
+	const square = firstNumber ** secondNumber
+  return square
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if(number == 0) {
+    return 1
+  }else {
+  const numbers = []
+  for(let i = 1;i <= number;i++) {
+    numbers.push(i)
+  }
+	const factorize = numbers.reduce((previousValue,currentValue) => 
+  previousValue * currentValue
+  )
+  return factorize
+}
 };
 
 // Do not edit below this line
