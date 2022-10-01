56036b996a8107c4346b674720a046cd951370ba
Mike Tagg <mdtagg91@gmail.com>
2022-10-01 15:10:18 -0700

    Exercise 9 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
-};
+    let splitString = string.split('')
+    
+    function filterCharacters(characters) {
+      if(characters.match(/\w/) !== null) {
+        return true
+      }else {
+        return false
+      }
+    }
+    
+    const newArray = splitString.filter(filterCharacters).join('').toLowerCase()
+    const reversedString = newArray.split('').reverse().join('')
+
+    if(reversedString === newArray) {
+        return true
+    }else {
+        return false
+    }
+  };
+  
 
 // Do not edit below this line
 module.exports = palindromes;
