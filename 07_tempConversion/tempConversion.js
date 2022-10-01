13c82c2423c39981dfe84912b79044880a24ca29
Mike Tagg <mdtagg91@gmail.com>
2022-10-01 11:27:27 -0700

    Exercise 7 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  const fahrenheit = ((temp - 32) * (5/9)).toFixed(1)
+  return parseFloat(fahrenheit)
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  const celcius = (temp * (9/5) + 32).toFixed(1)
+  return parseFloat(celcius)
 };
 
 // Do not edit below this line
