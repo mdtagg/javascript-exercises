88ba24462fb830683c64db9e4f2500812e90c13a
Mike Tagg <mdtagg91@gmail.com>
2022-10-01 09:11:21 -0700

    Exercise 5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(firstNumber,secondNumber) {
 
+    if(firstNumber < 0 || typeof(firstNumber) != 'number' ||
+        secondNumber < 0 || typeof(secondNumber) != 'number') {
+        return 'ERROR'
+    }
+    // }else if(typeof(firstNumber) !== number || typeof(secondNumber !== number)) {
+    //     return 'ERROR'
+    // }
+
+    let iterations = 0
+    let startNumber = 0
+    let sum = 0
+
+    if(firstNumber > secondNumber) {
+        iterations = firstNumber - secondNumber + 1
+        startNumber = secondNumber
+    } else {
+        iterations = secondNumber - firstNumber + 1
+        startNumber = firstNumber
+    }
+
+    for(let i = 0;i < iterations;i++) {
+        sum += startNumber;
+        startNumber++
+    }
+    return sum
 };
 
 // Do not edit below this line
