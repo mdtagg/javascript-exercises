8bf889cd987d5df8c3086dbfdf4e82e0da891e71
Mike Tagg <mdtagg91@gmail.com>
2022-10-01 21:29:25 -0700

    Exercise 10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(number) {
+    let previousValue = 0
+    let nextValue = 1
+    let fibNumbers = []
+    
+    if(number < 0) {
+        return 'OOPS'
+    }else {
+    for(let i = 0; i < number;i++) {
+      fibNumbers.push(nextValue)
+      nextValue = nextValue + previousValue
+      previousValue = nextValue - previousValue
+    }
+    const fibNumber = fibNumbers[number - 1]
+    return fibNumber
+}
+  };
 
 // Do not edit below this line
 module.exports = fibonacci;
