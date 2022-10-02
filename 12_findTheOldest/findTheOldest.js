bce07f9cdecea02acb00e942203db6dd1289e4b6
Mike Tagg <mdtagg91@gmail.com>
2022-10-02 14:57:54 -0700

    Exercise 12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    const today = new Date()
+        people.map(person => {
+        if(!person.yearOfDeath) {
+        person['yearsAlive'] = today.getFullYear() - person.yearOfBirth
+        }else {
+        person['yearsAlive'] = person.yearOfDeath - person.yearOfBirth
+        }
+        })
+    
+    let oldestAge = people[0].yearsAlive
+    let index = 0
+    for(let i = 0;i < people.length;i++) {
+        if(people[i].yearsAlive > oldestAge) {
+        oldestAge = people[i].yearsAlive
+        index = i
+        }
+    }
+    const oldestPerson = people[index]
+    return oldestPerson
+    }
 
 // Do not edit below this line
 module.exports = findTheOldest;
